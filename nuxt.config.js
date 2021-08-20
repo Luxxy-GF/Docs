32fc0eef617c2621d350fc9621f005adc7d688ac
luxxy-gf <deniedaddicted@gmail.com>
2021-08-20 13:44:12 +0000

    (:

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,27 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from '@nuxt/content-theme-docs';
 
 export default theme({
-  docs: {
-    primaryColor: '#E24F55',
-    router: {
-      base: '/docs/'
-    }
-  }
-})
+  loading: { color: '#48bb78' },
+  generate: {
+    fallback: '404.html', // for Netlify
+    routes: ['/'] // give the first url to start crawling
+  },
+  router: {
+    base: '/docs/'
+  },
+  i18n: {
+    locales: () => [
+      {
+        code: 'en',
+        iso: 'en-US',
+        file: 'en-US.js',
+        name: 'English'
+      }
+    ],
+    defaultLocale: 'en'
+  },
+  content: {
+    liveEdit: false
+  },
+  buildModules: []
+});
